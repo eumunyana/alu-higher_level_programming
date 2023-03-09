f3e296a96637d8a2c8333664099226a3fe637ec3
eumunyana <e.umunyana@alustudent.com>
2023-03-09 04:37:44 -0800

    Task-3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0")
     else:
-        for i in range(1, len([argv])):
+        for i in range(1, len(argv)):
             result += int(argv[i])
-            print("{}".format(result))
+
+        print(result)
