4c04378e91fb4458da5da016bb8a26eeb86342a3
eumunyana <e.umunyana@alustudent.com>
2023-03-09 04:13:18 -0800

    Task-2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         print("1: {:s}".format(argv[1]))
     else:
         print("{:d} arguments:".format(len(argv) - 1))
-        for 1  in range(1, len(argv)):
+        for i in range(1, len(argv)):
             print("{:d}: {:s}".format(i, argv[i]))
