7a50894a9ad290cede53d0963fa662994c12bc54
eumunyana <e.umunyana@alustudent.com>
2023-03-09 03:31:59 -0800

    Task-0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
-from add_0 import add
+#!/usr/bin/python
+
 if __name__ == "___main__":
+    from add_0 import add
+    
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
