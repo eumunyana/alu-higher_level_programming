70b414adfe0f621da663b1bee40de75fd0a061db
eumunyana <e.umunyana@alustudent.com>
2023-03-10 09:51:26 -0800

    Task-10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
     for i in range(len(new_list)):
-        if new_list[i] % 2== 0:
+        if new_list[i] % 2 == 0:
             new_list[i] = True
         else:
             new_list[i] = False
