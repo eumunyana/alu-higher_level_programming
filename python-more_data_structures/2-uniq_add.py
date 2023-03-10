339ffafd7c97c4704090a9fcbe9c64a67ad20070
eumunyana <e.umunyana@alustudent.com>
2023-03-10 11:19:20 -0800

    Task-2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -5,6 +5,6 @@ def uniq_add(my_list=[]):
     for item in my_list:
         if item not in res_list:
             res_list.append(item)
-        for uniqs in res_list:
-            res += uniqs
-        return res
+    for uniqs in res_list:
+        res += uniqs
+    return res
