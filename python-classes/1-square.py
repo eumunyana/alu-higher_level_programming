8a5611ab8a5d0244468e3a9ebb6f0a3e3e382f48
eumunyana <e.umunyana@alustudent.com>
2023-03-16 07:58:28 -0700

    Task-1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This module creates an empty class named Square"""
+
+
+class Square:
+    """An empty class
+    Attributes:
+    attr1 (size): size of square
+    """
+
+    def__init__(self, size):
+        """"
+
+        Args:
+        size: size for __size attribute of class instance
+        """
+        self.__size = size
