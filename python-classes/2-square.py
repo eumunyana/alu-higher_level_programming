1eb8a1ccbb2f89b6e2aa7cacc317f50a2c1501f0
eumunyana <e.umunyana@alustudent.com>
2023-03-16 08:56:45 -0700

    task-2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,20 +3,28 @@
 
 
 class Square:
-    """A class named Square
-    Attributes:
-    attr1 (size): size of square
     """
+    Square class: defines a square by a private instance attribute size.
 
-    def__init__(self, size=0):
+    Attributes:
+        __size (int): size of the square.
+
+    """
+    def __init__(self, size=0):
         """
+        Initializes a square instance with an optional size.
 
         Args:
-        size(int):size for __size attribute of class instance
-        """
+            size (int): size of the square. Defaults to 0.
 
-        if type(size) != int:
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
