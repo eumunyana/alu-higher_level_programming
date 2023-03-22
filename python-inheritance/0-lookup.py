1b01988c3f1824dbbe8029282469619cc0a61400
eumunyana <e.umunyana@alustudent.com>
2023-03-22 05:06:46 -0700

    Task-0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,6 @@
+def lookup(obj):
+    """
+    This function takes an object as an argument and returns a list of all the
+    attributes and methods available for that object.
+    """
+    return [attr for attr in dir(obj)]
