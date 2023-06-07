e09494c30c736ac522bfee73fda456fba10f3083
eumunyana <e.umunyana@alustudent.com>
2023-06-07 07:14:04 -0700

    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

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Prints the first arguments to it
 
-if (process.argv[2] === undefinied) {
+if (process.argv[2] === undefined) {
 	console.log('No  argument');	
 } else {
 	console.log(process.argv[2]);
