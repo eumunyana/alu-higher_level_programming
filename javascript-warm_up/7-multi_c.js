860d0e4ad4279bb99b4e63cd2c801be690512979
eumunyana <e.umunyana@alustudent.com>
2023-06-08 02:30:17 -0700

    7. I love C

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -6,7 +6,7 @@ const lang = 'C is fun';
 if (isNaN(process.argv[2])) {
 	console.log('Missing number of occurences');
 } else {
-	for (let i = 0; i < parseInt(process.argv[2]; i++) {
+	for (let i = 0; i < parseInt(process.argv[2]); i++) {
 		console.log(lang);
 	}
 }
