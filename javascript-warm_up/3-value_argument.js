af906ba4ee6665f0d082800f7a99f056e229d704
eumunyana <e.umunyana@alustudent.com>
2023-06-08 01:36:09 -0700

    Updating the 3-value_argument file

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-// Prints the first arguments to it
+// prints the first argument passed to it
 
 if (process.argv[2] === undefined) {
-	console.log('No argument');	
+	console.log('No argument');
 } else {
 	console.log(process.argv[2]);
 }
