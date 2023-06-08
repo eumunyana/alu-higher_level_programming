999f040dd7b39a330fa1f56fa2ac5062a3236dff
eumunyana <e.umunyana@alustudent.com>
2023-06-08 02:14:34 -0700

    6. Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints 3 lines: (like 1-multi_langauges.js) but by using an array of string and a loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
