0310e96e5f62cdde51061ca658ca4668849fbf85
eumunyana <e.umunyana@alustudent.com>
2023-06-08 01:44:41 -0700

    4. Create a sentence

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-//prints two argument passed to it in this format: " is "
+// prints two argument passed to it in this format: " is "
 
 console.log(process.argv[2] + ' is ' + process.argv[3]);
