ab553cf1ef15979f73883b2982fb6c553fcd4103
eumunyana <e.umunyana@alustudent.com>
2023-06-08 04:15:18 -0700

    11. Second biggest!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.lenght)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
